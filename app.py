1a935c5a8a7077a814317f8d1e12a72cdb3e67e6
chrisjho <27cho@athenian.org>
2026-01-30 10:29:41 -0800

    Done

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('profile.html')
+
+@app.route('/clicker')
+def a():
+    return render_template('clicker.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
